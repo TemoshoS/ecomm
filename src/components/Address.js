1f133d441ad7d953397b761a39230ec1b337dbe3
TemoshoS <temoshomaduane@gmail.vom>
2023-09-29 10:08:37 +0200

    address

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,10 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function Address() {
+export default function Address({ address, onSaveAddress }) {
+  // Initialize the local state with the passed address
+  const [street, setStreet] = useState(address.street);
+  const [suburb, setSuburb] = useState(address.suburb);
+  const [province, setProvince] = useState(address.province);
+  const [postcode, setPostcode] = useState(address.postcode);
+
+  // Update the local state if the address prop changes
+  useEffect(() => {
+    setStreet(address.street);
+    setSuburb(address.suburb);
+    setProvince(address.province);
+    setPostcode(address.postcode);
+  }, [address]);
+
+  // Function to handle saving the address
+ const handleSaveAddress = () => {
+  console.log('Saving address...', street, suburb, province, postcode);
+  const newAddress = {
+    street,
+    suburb,
+    province,
+    postcode,
+  };
+  console.log('New Address:', newAddress);
+  onSaveAddress(newAddress);
+};
+
+  
+
   return (
-    <View>
-      <Text>Address</Text>
-    </View>
-  )
-} 
+    <div className='address'>
+      <div className="address-container">
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" value={street} onChange={(e) => setStreet(e.target.value)} />
+      </div>
+
+      <div className="address-container">
+        <label htmlFor="suburb">Suburb</label>
+        <input type='text' id="suburb" value={suburb} onChange={(e) => setSuburb(e.target.value)} />
+      </div>
+
+      <div className="address-container">
+        <label htmlFor="province">Province</label>
+        <input type="text" id="province" value={province} onChange={(e) => setProvince(e.target.value)} />
+      </div>
+
+      <div className="address-container">
+        <label htmlFor="postcode">Postcode</label>
+        <input type='text' id="postcode" value={postcode} onChange={(e) => setPostcode(e.target.value)} />
+      </div>
+      <button onClick={handleSaveAddress}>Save</button>
+    </div>
+  );
+}
--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Address from './Address';
 
 export const Checkout = ({ cartItems, totalPrice }) => {
+  // State to store the address
+  const [address, setAddress] = useState({
+    street: '',
+    suburb: '',
+    province: '',
+    postcode: 0,
+  });
+
+  // Function to save the address
+  const saveAddress = (newAddress) => {
+    setAddress(newAddress);
+  };
+
   return (
     <div className='c'>
+
+<Address address={address} onSaveAddress={saveAddress} />
+
       <h2>Checkout</h2>
       <ul>
         {cartItems.map((item) => (
@@ -12,6 +29,7 @@ export const Checkout = ({ cartItems, totalPrice }) => {
         ))}
       </ul>
       <p>Total Price: R {totalPrice()}</p>
+      <button>Confirm</button>
     </div>
   );
 };
