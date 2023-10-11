08914b1be31cda9ac9ccfd27c5b81274541af59a
TemoshoS <temoshomaduane@gmail.com>
2023-10-11 15:01:46 +0200

    Validation

--- src/components/login.js
+++ src/components/login.js
@@ -12,6 +12,9 @@ const Signin = () => {
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
     const [rememberMe, setRememberMe] = useState(false); // State to remember the "Remember Me" checkbox
+    const [usererror, setUserError] = useState(null);
+    const [passerror, setPassError] = useState(null);
+    const [error, setError] = useState(null);
     const navigate = useNavigate();
 
     // Check if there is a saved email cookie and set it as the default value
@@ -30,13 +33,25 @@ const Signin = () => {
 
     // Function to handle the login process
     const login = () => {
+
+        if (!email || !password) {
+            alert('Please fill in all the required fields.');
+            return;
+        }
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                console.log(userCredential);
+                setUserError(null);
                 navigate('/checkout'); // Redirect to the checkout page upon successful login
             })
             .catch((error) => {
-                console.log(error.message); // Log any errors that occur during login
+                if (error.code === 'auth/user-not-found') {
+                    setUserError('User does not exists.');
+                } else if (error.code === 'auth/wrong-password') {
+                    setPassError('Wrong password.');
+                } else {
+                    setError(error.message); 
+                }
+            
             });
 
         // If "Remember Me" is checked, save the email as a cookie
@@ -66,6 +81,8 @@ const Signin = () => {
                 <p className="heading">
                     <BiSolidUserCircle />
                 </p>
+               
+               
                 <div className='input-container'>
                     <label>Email</label>
                     <input
@@ -74,6 +91,9 @@ const Signin = () => {
                         onChange={(event) => setEmail(event.target.value)}
                     />
                 </div>
+
+                {usererror && <div className="error-message">{usererror}</div>}
+
                 <div className='input-container'>
                     <label>Password</label>
                     <div className='password-input-container'>
@@ -88,6 +108,7 @@ const Signin = () => {
                             {showPassword ? <FaEyeSlash /> : <FaEye />}
                         </i>
                     </div>
+                    {passerror && <div className="error-message">{passerror}</div>}
                 </div>
                 <div>
                     <label>
@@ -97,16 +118,21 @@ const Signin = () => {
                             checked={rememberMe}
                             onChange={handleRememberMeChange}
                         />
-                        Remember me &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <Link to='/forgotpassword' style={{ color: 'red', textDecoration: 'none' }}>
+                     <span className='remeber'>  Remember me
+                        <Link to='/forgotpassword' style={{ color: 'red', textDecoration: 'none' , marginLeft:'50px'}}>
                             Forgot password
-                        </Link>
+                        </Link></span> 
                     </label>
                 </div>
+
+                
+
                 <button className='submit-button' onClick={login}>
                     Login
                 </button>
-                <p>
+
+                {error && <div className="error-message">{error}</div>}
+                <p className='account-yet'>
                     Don't have an account?{' '}
                     <Link to='/register' style={{ color: 'red', textDecoration: 'none' }}>
                         Sign up
